ef16a979ba9a61a7505333b917a39199b2af6d07
Tu Nombre <tu_correo@example.com>
2024-11-26 16:02:16 -0600

    .git add

---
+++ FGR.js
@@ -0,0 +1,3 @@
+document.getElementById("report-button").addEventListener("click", () => {
+    alert("Gracias por tu cooperación. Puedes reportar al 911 o visitar el sitio oficial de la Fiscalía.");
+});
